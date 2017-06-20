859c826a74aebe05007c9895653bb318c903c383
Iamsdt <shudiptotrafder@gmail.com>
2017-06-20 21:36:57 +0600

    new code

---
+++ src/anothercourse/Ovveride.kt
@@ -0,0 +1,51 @@
+package anothercourse
+
+/**
+ * Created by Shudipto Trafder on 6/20/2017.
+ */
+
+open class Math3 {
+
+    open fun add(a:Int, b:Int):Int{
+        return a+b
+    }
+
+    open fun sub(a:Int, b:Int):Int{
+        return a-b
+    }
+}
+
+class Math4(val extra:Int) : Math3(){
+
+    override fun add(a:Int, b:Int):Int{
+        return a+b+extra
+    }
+
+    override fun sub(a:Int, b:Int):Int{
+        super.sub(a, b)
+        return a-b+extra
+    }
+
+    fun mul(a:Int, b:Int):Int{
+        return a*b
+    }
+
+    fun div(a:Int, b:Int):Int{return a/b}
+}
+
+
+
+fun main(args: Array<String>){
+
+//    val math = Math()
+//    println("add: ${math.add(20,21)}")
+//    println("add: ${math.sub(22,21)}")
+
+    val math2 = Math4(10)
+    println("add: ${math2.add(20,21)}")
+    println("sub: ${math2.sub(22,21)}")
+
+    println("mul: ${math2.mul(20,21)}")
+    println("div: ${math2.div(22,21)}")
+
+}
